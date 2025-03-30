c5698f1bbbcaca3289c7c88c30ca7058b2058210
mguffi <116266122+mguffi@users.noreply.github.com>
2025-03-30 17:35:27 +0200

    refactor app.js to register people route for improved routing structure

--- myapp/app.js
+++ myapp/app.js
@@ -15,6 +15,7 @@ var authRouter = require('./routes/auth');
 // Router für die Profilseite
 var profilRouter = require('./routes/profil');
 const likesRouter = require('./routes/likes'); // Importiere die Likes-Route
+const peopleRouter = require('./routes/people'); // Importiere die People-Route
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -40,6 +41,7 @@ app.use('/users', usersRouter);
 app.use('/', authRouter);
 app.use('/profil', profilRouter);
 app.use('/likes', likesRouter); // Registriere die Likes-Route
+app.use('/people', peopleRouter); // Registriere die People-Route
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

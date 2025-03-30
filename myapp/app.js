ee8b09046f759c0eac7b67a2c10d4b36dbf1f3a5
mguffi <116266122+mguffi@users.noreply.github.com>
2025-03-30 17:14:51 +0200

    update session secret to a more secure value

--- myapp/app.js
+++ myapp/app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Session-Middleware konfigurieren
 app.use(session({
-    secret: '123', // Ersetze durch einen sicheren Wert
+    secret: 'deinGeheimerSchlüssel', // Ersetze durch einen sicheren Wert
     resave: false,
     saveUninitialized: true,
 }));
